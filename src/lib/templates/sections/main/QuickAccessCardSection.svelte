196f1e0a742c2eda8624a577faacd953925466f3
Marko Gjorgjievski <markodjordjievski@gmail.com>
2023-10-29 02:03:41 +0200

    home page

---
+++ src/lib/templates/sections/main/QuickAccessCardSection.svelte
@@ -0,0 +1,10 @@
+<script>
+	import CategoryCardGrid from './CategoryCardGrid.svelte';
+	import CityCardGrid from './CityCardGrid.svelte';
+</script>
+
+<div class="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-24 mx-auto flex flex-col gap-6">
+	<CategoryCardGrid />
+	<hr class=" dark:border-gray-800" />
+	<CityCardGrid />
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,9 @@
 <script>
 	import '../app.css';
+	import Header from '$lib/templates/sections/header/Header.svelte';
+	import Footer from '$lib/templates/sections/footer/Footer.svelte';
 </script>
 
+<Header />
 <slot />
+<Footer />
