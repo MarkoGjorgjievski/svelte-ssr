78ce6bfb8e15b854f6675d617c3f0884202adb35
Marko Gjorgjievski <markodjordjievski@gmail.com>
2023-10-30 12:41:04 +0100

    dashboard sidebar

--- src/lib/components/base/accordion/AccordionSidebarItem.svelte
+++ src/lib/components/base/accordion/AccordionSidebarItem.svelte
@@ -1,19 +1,19 @@
 <script lang="ts">
-	import AccordionIcons from './AccordionIcons.svelte';
+	import { twMerge } from 'tailwind-merge';
 
 	export let icon: string = '';
 	export let label: string = '';
-	export let hasChildren: boolean = false;
 	export let slug: string = '';
+	export let active: boolean = false;
 </script>
 
 <a
-	class="hs-accordion-toggle flex items-center gap-x-3.5 py-2 px-2.5 hs-accordion-active:text-blue-600 hs-accordion-active:hover:bg-transparent text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300 dark:hs-accordion-active:text-white"
+	class={twMerge(
+		'flex items-center gap-x-3.5 py-2 px-2.5  text-sm text-slate-700 rounded-md hover:bg-gray-100 dark:bg-gray-800 dark:hover:bg-gray-900 dark:text-slate-400 dark:hover:text-slate-300 ',
+		active && 'text-blue-600 hover:bg-transparent dark:text-white'
+	)}
 	href={slug}
 >
 	<i class={icon} />
 	{label}
-	{#if hasChildren}
-		<AccordionIcons />
-	{/if}
 </a>
--- src/lib/templates/dashboard/DashboardSidebar.svelte
+++ src/lib/templates/dashboard/DashboardSidebar.svelte
@@ -1,8 +1,17 @@
 <script>
-	import ThemeSwitcher from '$lib/components/ThemeSwitcher.svelte';
+	import { page } from '$app/stores';
 	import AccordionSidebarItem from '$lib/components/base/accordion/AccordionSidebarItem.svelte';
 
 	const baseUrl = '/dashboard';
+
+	const routes = [
+		{ label: 'Dashboard', icon: 'house', slug: baseUrl },
+		{ label: 'Calendar', icon: 'calendar-week', slug: `${baseUrl}/calendar` },
+		{ label: 'Account', icon: 'person-vcard', slug: `${baseUrl}/account` },
+		{ label: 'Staff', icon: 'people', slug: `${baseUrl}/staff` },
+		{ label: 'Analytics', icon: 'bar-chart', slug: `${baseUrl}/analytics` },
+		{ label: 'Clients', icon: 'stars', slug: `${baseUrl}/clients` }
+	];
 </script>
 
 <div
@@ -10,29 +19,22 @@
 	class="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 left-0 bottom-0 z-[60] w-64 bg-white border-r border-gray-200 pt-7 pb-10 overflow-y-auto scrollbar-y lg:block lg:translate-x-0 lg:right-auto lg:bottom-0 dark:scrollbar-y dark:bg-gray-800 dark:border-gray-700"
 >
 	<div class="px-6">
-		<a class="flex-none text-xl font-semibold dark:text-white" href="/" aria-label="Brand">Brand</a>
+		<a class="flex-none text-xl font-semibold dark:text-white" href="/" aria-label="Brand">Taman</a>
+		<p class="flex-none text-sm text-gray-600 dark:text-gray-400">Alley Barber Shop</p>
 	</div>
 
 	<nav class="hs-accordion-group p-6 w-full flex flex-col flex-wrap" data-hs-accordion-always-open>
 		<ul class="space-y-1.5">
-			<li><AccordionSidebarItem label="Dashboard" icon="bi bi-house" slug={baseUrl} /></li>
-			<li>
-				<AccordionSidebarItem
-					label="Calendar"
-					icon="bi bi-calendar-week"
-					slug="{baseUrl}/calendar"
-				/>
-			</li>
-			<li>
-				<AccordionSidebarItem label="Account" icon="bi bi-person-vcard" slug="{baseUrl}/account" />
-			</li>
-			<li><AccordionSidebarItem label="Staff" icon="bi bi-people" slug="{baseUrl}/staff" /></li>
-			<li>
-				<AccordionSidebarItem label="Analytics" icon="bi bi-bar-chart" slug="{baseUrl}/analytics" />
-			</li>
-			<li>
-				<AccordionSidebarItem label="Clients" icon="bi bi-stars" slug="{baseUrl}/clients" />
-			</li>
+			{#each routes as { label, icon, slug }}
+				<li>
+					<AccordionSidebarItem
+						{label}
+						icon="bi bi-{icon}"
+						{slug}
+						active={$page.url.pathname === slug}
+					/>
+				</li>
+			{/each}
 		</ul>
 	</nav>
 </div>
