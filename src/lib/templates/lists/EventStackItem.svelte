9c3918fa3228ed7c03987bb5774779c7ed757e9a
Marko Gjorgjievski <markodjordjievski@gmail.com>
2023-10-31 01:40:36 +0100

    event stack

---
+++ src/lib/templates/lists/EventStackItem.svelte
@@ -0,0 +1,64 @@
+<!-- Card -->
+<div
+	class="bg-white border border-gray-200 shadow-sm rounded-xl dark:bg-gray-800 dark:border-gray-700 dark:shadow-slate-700/[.7]"
+>
+	<div class="flex justify-between gap-2 p-3">
+		<div>
+			<div class="flex gap-2 text-xs items-center mb-2 text-gray-800 dark:text-gray-300">
+				<i class="bi bi-calendar-event text-gray-400" /><span>Monday, 23/10/2023</span>
+				<i class="bi bi-clock text-gray-400" /><span>15:45</span>
+			</div>
+			<div class="flex gap-2">
+				<img
+					class="h-12 w-12 flex-none rounded-full bg-gray-50"
+					src="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+					alt=""
+				/>
+				<div>
+					<span class="block text-xs mt-1 font-semibold uppercase text-blue-600 dark:text-blue-500">
+						Women's haircut
+					</span>
+					<h3 class=" font-medium text-gray-800 dark:text-gray-300 dark:hover:text-white">
+						Andrea Underwood
+					</h3>
+				</div>
+			</div>
+		</div>
+		<div class="flex justify-end gap-x-2 h-fit">
+			<!-- <button
+					type="button" -->
+			<button
+				type="button"
+				class="inline-flex flex-shrink-0 h-[1.875rem] w-[1.875rem] justify-center items-center gap-2 rounded-md border font-medium bg-white text-red-600 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-red-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
+			>
+				<i class="bi bi-x-lg" />
+			</button>
+			<!-- <button
+					type="button" -->
+			<button
+				type="button"
+				class="inline-flex flex-shrink-0 h-[1.875rem] w-[1.875rem] justify-center items-center gap-2 rounded-md border font-medium bg-white text-green-600 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-green-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
+			>
+				<i class="bi bi-check-lg" />
+			</button>
+			<!-- </button> -->
+		</div>
+	</div>
+	<!-- <div
+		class="mt-auto flex border-t border-gray-200 divide-x divide-gray-200 dark:border-gray-700 dark:divide-gray-700"
+	>
+		<a
+			class="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-bl-xl font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm sm:p-4 dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
+			href="#"
+		>
+			View sample
+		</a>
+		<a
+			class="w-full py-3 px-4 inline-flex justify-center items-center gap-2 rounded-br-xl font-medium bg-white text-gray-700 shadow-sm align-middle hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-blue-600 transition-all text-sm sm:p-4 dark:bg-slate-900 dark:hover:bg-slate-800 dark:border-gray-700 dark:text-gray-400 dark:hover:text-white dark:focus:ring-offset-gray-800"
+			href="#"
+		>
+			View API
+		</a>
+	</div> -->
+</div>
+<!-- End Card -->
--- src/routes/(app)/dashboard/calendar/+page.svelte
+++ src/routes/(app)/dashboard/calendar/+page.svelte
@@ -1,7 +1,46 @@
 <script>
 	import Calendar from '$lib/templates/dashboard/calendar/Calendar.svelte';
+	import EventStackItem from '$lib/templates/lists/EventStackItem.svelte';
+	import { fly, slide } from 'svelte/transition';
+
+	let isOpen = true;
 </script>
 
 <div class="py-4">
-	<Calendar />
+	<div class="flex w-full">
+		<div class="{isOpen ? 'w-3/4' : 'w-0'} transition-all duration-300">
+			{#if isOpen}
+				<div class="pr-8" transition:fly={{ x: -100, duration: 300 }}>
+					<h2 class="text-xl font-semibold text-gray-800 dark:text-gray-200 mb-8 mt-2">
+						Upcoming appointments
+					</h2>
+					<div class="flex flex-col gap-4" transition:slide={{ duration: 700, delay: 100 }}>
+						<EventStackItem />
+						<!-- <hr class=" dark:border-gray-800" /> -->
+
+						<EventStackItem />
+						<!-- <hr class=" dark:border-gray-800" /> -->
+
+						<EventStackItem />
+						<!-- <hr class=" dark:border-gray-800" /> -->
+
+						<EventStackItem />
+					</div>
+				</div>
+			{/if}
+		</div>
+		<div class="w-full relative">
+			<button
+				class="absolute top-10 flex justify-center items-center w-5 h-5 border border-gray-200 group-hover:bg-gray-200 text-gray-400 rounded dark:border-gray-700 dark:group-hover:bg-gray-700 dark:text-gray-400"
+				on:click={() => (isOpen = !isOpen)}
+			>
+				{#if isOpen}
+					<i class="bi bi-arrow-bar-left" />
+				{:else}
+					<i class="bi bi-arrow-bar-right" />
+				{/if}
+			</button>
+			<Calendar />
+		</div>
+	</div>
 </div>
