78cc377fe474a25da7ebae94db38a4a774e50fa6
Marko Gjorgjievski <markodjordjievski@gmail.com>
2023-11-02 22:47:36 +0100

    categories

---
+++ src/lib/templates/sections/galleries/CardsOnImages.svelte
@@ -0,0 +1,198 @@
+<!-- Masonry Cards -->
+<div class="max-w-[85rem] px-4 py-4 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+	<!-- Grid -->
+
+	<div class="grid grid-cols-2 md:grid-cols-4 gap-4">
+		<div class="grid gap-4">
+			<a
+				href="categories/cat"
+				class="relative hover:shadow-lg rounded-lg transition-all duration-300"
+			>
+				<span
+					class="absolute bottom-1 left-1 backdrop-filter backdrop-blur-sm m-0.5 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200"
+				>
+					Category name
+				</span>
+				<img
+					class="h-auto max-w-full rounded-lg"
+					src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image.jpg"
+					alt=""
+				/>
+			</a>
+			<a
+				href="categories/cat"
+				class="relative hover:shadow-lg rounded-lg transition-all duration-300"
+			>
+				<span
+					class="absolute bottom-1 left-1 backdrop-filter backdrop-blur-sm m-0.5 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200"
+				>
+					Category name
+				</span>
+				<img
+					class="h-auto max-w-full rounded-lg"
+					src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-1.jpg"
+					alt=""
+				/>
+			</a>
+			<a
+				href="categories/cat"
+				class="relative hover:shadow-lg rounded-lg transition-all duration-300"
+			>
+				<span
+					class="absolute bottom-1 left-1 backdrop-filter backdrop-blur-sm m-0.5 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200"
+				>
+					Category name
+				</span>
+				<img
+					class="h-auto max-w-full rounded-lg"
+					src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-2.jpg"
+					alt=""
+				/>
+			</a>
+		</div>
+		<div class="grid gap-4">
+			<a
+				href="categories/cat"
+				class="relative hover:shadow-lg rounded-lg transition-all duration-300"
+			>
+				<span
+					class="absolute bottom-1 left-1 backdrop-filter backdrop-blur-sm m-0.5 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200"
+				>
+					Category name
+				</span>
+				<img
+					class="h-auto max-w-full rounded-lg"
+					src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-3.jpg"
+					alt=""
+				/>
+			</a>
+			<a
+				href="categories/cat"
+				class="relative hover:shadow-lg rounded-lg transition-all duration-300"
+			>
+				<span
+					class="absolute bottom-1 left-1 backdrop-filter backdrop-blur-sm m-0.5 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200"
+				>
+					Category name
+				</span>
+				<img
+					class="h-auto max-w-full rounded-lg"
+					src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-4.jpg"
+					alt=""
+				/>
+			</a>
+			<a
+				href="categories/cat"
+				class="relative hover:shadow-lg rounded-lg transition-all duration-300"
+			>
+				<span
+					class="absolute bottom-1 left-1 backdrop-filter backdrop-blur-sm m-0.5 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200"
+				>
+					Category name
+				</span>
+				<img
+					class="h-auto max-w-full rounded-lg"
+					src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-5.jpg"
+					alt=""
+				/>
+			</a>
+		</div>
+		<div class="grid gap-4">
+			<a
+				href="categories/cat"
+				class="relative hover:shadow-lg rounded-lg transition-all duration-300"
+			>
+				<span
+					class="absolute bottom-1 left-1 backdrop-filter backdrop-blur-sm m-0.5 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200"
+				>
+					Category name
+				</span>
+				<img
+					class="h-auto max-w-full rounded-lg"
+					src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-6.jpg"
+					alt=""
+				/>
+			</a>
+			<a
+				href="categories/cat"
+				class="relative hover:shadow-lg rounded-lg transition-all duration-300"
+			>
+				<span
+					class="absolute bottom-1 left-1 backdrop-filter backdrop-blur-sm m-0.5 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200"
+				>
+					Category name
+				</span>
+				<img
+					class="h-auto max-w-full rounded-lg"
+					src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-7.jpg"
+					alt=""
+				/>
+			</a>
+			<a
+				href="categories/cat"
+				class="relative hover:shadow-lg rounded-lg transition-all duration-300"
+			>
+				<span
+					class="absolute bottom-1 left-1 backdrop-filter backdrop-blur-sm m-0.5 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200"
+				>
+					Category name
+				</span>
+				<img
+					class="h-auto max-w-full rounded-lg"
+					src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-8.jpg"
+					alt=""
+				/>
+			</a>
+		</div>
+		<div class="grid gap-4">
+			<a
+				href="categories/cat"
+				class="relative hover:shadow-lg rounded-lg transition-all duration-300"
+			>
+				<span
+					class="absolute bottom-1 left-1 backdrop-filter backdrop-blur-sm m-0.5 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200"
+				>
+					Category name
+				</span>
+				<img
+					class="h-auto max-w-full rounded-lg"
+					src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-9.jpg"
+					alt=""
+				/>
+			</a>
+			<a
+				href="categories/cat"
+				class="relative hover:shadow-lg rounded-lg transition-all duration-300"
+			>
+				<span
+					class="absolute bottom-1 left-1 backdrop-filter backdrop-blur-sm m-0.5 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200"
+				>
+					Category name
+				</span>
+				<img
+					class="h-auto max-w-full rounded-lg"
+					src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-10.jpg"
+					alt=""
+				/>
+			</a>
+			<a
+				href="categories/cat"
+				class="relative hover:shadow-lg rounded-lg transition-all duration-300"
+			>
+				<span
+					class="absolute bottom-1 left-1 backdrop-filter backdrop-blur-sm m-0.5 inline-flex items-center gap-1.5 py-2 px-3 rounded-full text-sm bg-gray-100 text-gray-800 dark:bg-gray-800 dark:text-gray-200"
+				>
+					Category name
+				</span>
+				<img
+					class="h-auto max-w-full rounded-lg"
+					src="https://flowbite.s3.amazonaws.com/docs/gallery/masonry/image-11.jpg"
+					alt=""
+				/>
+			</a>
+		</div>
+	</div>
+
+	<!-- End Grid -->
+</div>
+<!-- End Masonry Cards -->
---
+++ src/routes/(marketing)/categories/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import CardsOnImages from '$lib/templates/sections/galleries/CardsOnImages.svelte';
+</script>
+
+<CardsOnImages />
