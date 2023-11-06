bc8aa00816b471cb9d19667b935f6b061dcbb3e0
Marko Gjorgjievski <markodjordjievski@gmail.com>
2023-11-07 00:19:14 +0100

    lang added

--- src/routes/(marketing)/categories/+page.svelte
+++
@@ -1,6 +0,0 @@
-<script>
-	import CardsOnImages from "$lib/views/shared/sections/galleries/CardsOnImages.svelte";
-
-</script>
-
-<CardsOnImages />
---
+++ src/routes/[[lang]]/(marketing)/categories/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+	import CardsOnImages from '$lib/views/shared/sections/galleries/CardsOnImages.svelte';
+</script>
+
+<CardsOnImages />
