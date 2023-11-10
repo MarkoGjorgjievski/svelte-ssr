16e0d1edaa7fea8538c5ab0aa8943a7ec4132e93
Marko Gjorgjievski <markodjordjievski@gmail.com>
2023-11-10 23:39:47 +0100

    checkbox

--- src/routes/(app)/dashboard/staff/+page.svelte
+++ src/routes/(app)/dashboard/staff/+page.svelte
@@ -1,7 +1,11 @@
 <script>
+	import Checkbox from '$lib/components/forms/checkbox/Checkbox.svelte';
 	import Input from '$lib/components/forms/input/Input.svelte';
 	import Select from '$lib/components/forms/select/Select.svelte';
 	import StaffSection from '$lib/views/dashboard/sections/staff/StaffSection.svelte';
+
+	let open = false;
+	$: console.log(open);
 
 	let options = [
 		{ label: '123', value: '1233' },
@@ -9,8 +13,9 @@
 	];
 </script>
 
-<div class="p-4">
+<div class="p-4 space-y-4">
 	<Input placeholder="Write something.." label="asmdlmsd" helperText="Helper text" disabled />
 	<Select {options} label="Select" />
+	<Checkbox label="Checkbox" bind:checked={open} id="check" />
 </div>
 <StaffSection />
